96c12966569d41ae713bb755c20830806fd7cbcb
Madhusha Prasad <madhushaprasad@Madhushas-MacBook-Air.local>
2024-10-24 11:55:00 +0530

    create default layout and image upload

---
+++ resources/views/livewire/image-upload.blade.php
@@ -0,0 +1,3 @@
+<div>
+   <div>Livewire image uploader</div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\ImageUpload;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('livewire-image-upload', ImageUpload::class);
